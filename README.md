66ea6859bda0832a6a2ec3c808c74ee9f43a7d91
WDwelle <willdwelle@frontier.com>
2025-01-13 16:06:20 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# My version of a text extraction node using RAG architecture
+
+(This project is based off the works of Mike Suliot)
+
---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+import hashlib
+import json
+import re
+import fnmatch
+import logging
+from config_manager import ConfigManager
+from data_loaders.data_loader_manager import DataLoaderManager
